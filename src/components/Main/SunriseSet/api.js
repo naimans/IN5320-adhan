0875b6c40a234251631b076f28c6d830e12322f6
Naima Noor Shorna <naimans@ifi.uio.no>
2019-09-27 23:00:57 +0200

    Update api.js

--- src/components/Main/SunriseSet/api.js
+++ src/components/Main/SunriseSet/api.js
@@ -1,13 +1,8 @@
 const latitude=59.9541049;
 const longitude=10.729883;
 const BASE_URL = 'https://api.sunrise-sunset.org/json?lat='+latitude+'&lng='+longitude+'&date=today';
-//const BASE_URL = 'https://api.sunrise-sunset.org/json?lat='+latitude+'&lng='+longitude+'&callback=mycallback';
-//const BASE_URL = 'https://fcc-weather-api.glitch.me/api/current?lat='+latitude+'&lon='+longitude;
-
-//const waitABit = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
 
 export async function getSunTime() {
-  //await waitABit(1000);
   const response = await fetch(`${BASE_URL}`);
   const result = await response.json();
   //console.log(result);
